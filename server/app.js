e92605c82895a6d6bc47cec8b462a80e33f3dcec
ibtissamoldi <ibtissamoldi@gmail.com>
2026-02-21 19:09:46 +0100

    sql queries in app.js

--- server/app.js
+++ server/app.js
@@ -56,8 +56,42 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // RUTES:
 
-app.get('/', (req, res) => {
-  res.render('index', common);
+// Ruta principal
+app.get('/', async (req, res) => {
+  try {
+    // 5 primeres pel·lícules + actors
+    const movies = await pool.query(`
+      SELECT 
+        f.film_id,
+        f.title,
+        f.release_year,
+        GROUP_CONCAT(CONCAT(a.first_name, ' ', a.last_name) SEPARATOR ', ') AS actors
+      FROM film f
+      JOIN film_actor fa ON f.film_id = fa.film_id
+      JOIN actor a ON fa.actor_id = a.actor_id
+      GROUP BY f.film_id
+      ORDER BY f.film_id
+      LIMIT 5
+    `);
+
+    // 5 primeres categories
+    const categories = await pool.query(`
+      SELECT name
+      FROM category
+      ORDER BY category_id
+      LIMIT 5
+    `);
+
+    res.render('index', {
+      ...common,
+      movies: movies[0],
+      categories: categories[0]
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.send('Error base de dades');
+  }
 });
 
 app.get('/movies', (req, res) => {
